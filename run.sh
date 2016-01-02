3176ed18689a6e8d04aeef6ae00bbe505360f30a
nanfenglei22 <nanfenglei22@163.com>
2016-01-02 17:25:43 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-./configure   --prefix="/home/lizhiming/tmp" \
+./configure   --prefix="/usr/local/openresty" \
               --with-cc-opt="-O0" \
 			  --with-luajit \
               --with-openssl=./bundle/openssl \
 			  --with-openssl-opt="-d"
 
+make && make install
