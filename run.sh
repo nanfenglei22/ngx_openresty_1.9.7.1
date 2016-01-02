ae88b19f3468f604efb517732294b0d0cf1bbaba
nanfenglei22 <nanfenglei22@163.com>
2016-01-02 17:18:52 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+./configure   --prefix="/home/lizhiming/tmp" \
+              --with-cc-opt="-O0" \
+			  --with-luajit \
+              --with-openssl=./bundle/openssl \
+			  --with-openssl-opt="-d"
+
